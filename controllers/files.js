946d57c41202b68f3d6c62d76869b33dd432b392
claire <claire.chang@starclinic.com.tw>
2022-04-27 21:57:54 +0800

    update

--- controllers/files.js
+++ controllers/files.js
@@ -2,41 +2,43 @@ const { successHandler, errorHandler } = require('../service/handler');
 const File = require('../models/filesModel');
 const dotenv = require('dotenv');
 dotenv.config({path:'../.env'});
-const { Upload } = require("@aws-sdk/lib-storage");
-const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
 
-const s3 = new S3Client({
-    region: process.env.AWS_DEFAULT_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
+aws.config.update({
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.AWS_DEFAULT_REGION
 });
+
+const s3 = new aws.S3();
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET,
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
+})
+
+const singleUpload = upload.single('image');
 
 const files = {
     postData: async (req, res) => {
         try {
-            const { file, file_name } = req.body;
-            console.log(11111);
-            console.log(file);
-            const params = {
-                Bucket: process.env.AWS_BUCKET,
-                Key: file_name,
-                Body: file
-            };
-            const data = await new Upload({
-                client: s3,
-                params: params
+            singleUpload(req, res, function(err, some) {
+                successHandler(res, "新增成功", req.file.location);
+                return successHandler(res, "新增成功", req.file.location);
             });
-            data.on("httpUploadProgress", (progress) => {
-                console.log(progress);
-            });
-            await data.done();
-            // const newFile = await File.create(data);
-            successHandler(res, "新增成功", data);
         } catch(error) {
-            console.log(error);
-            res.end();
+            errorHandler(res, error.message);
         }
     }
 }
