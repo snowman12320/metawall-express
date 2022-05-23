6e6abd6eac39d4f4a8c47e6e6999b789c161b193
claire <claire.chang@starclinic.com.tw>
2022-05-24 00:14:11 +0800

    feat: 更新 swagger 文件

--- routes/imgurFiles.js
+++ routes/imgurFiles.js
@@ -4,6 +4,31 @@ const imgurFiles = require('../controllers/imgurFiles');
 const { checkAuth, generateSendJWT } = require('../service/auth');
 
 router.post('', checkAuth, (req, res, next) => {
+    /**
+     * #swagger.tags = ['imgurFiles']
+     * #swagger.description = '上傳單張圖片'
+     * #swagger.parameters['Authorization'] = {
+            in: 'header',
+            type: 'string',
+            required: true,
+            description: 'Bearer token'
+        }
+     * #swagger.parameters['body'] = {
+            in: 'formData',
+            name: 'image',
+            type: 'file',
+            required: true,
+            description: '資料格式'
+        }
+     * #swagger.responses[200] = {
+            description: '圖片資訊',
+            schema: {
+                status: 'success',
+                message: '上傳成功',
+                data: 'https://i.imgur.com/t3ToMsx.jpg'
+            }
+        }
+     */
     imgurFiles.postData(req, res, next);
 });
 
